e2579e01c3d9f890e9b40b983d6dcc1be9394d83
Taciano <taciano3003@gmail.com>
2023-07-24 17:06:27 -0300

    function for selecting round time implemented, same strategy as in the spy player selection.

--- charades.cpp
+++ charades.cpp
@@ -1,1 +1,34 @@
 #include "charades.h"
+
+// prompts the user to input the amount of seconds each round lasts
+int set_round_duration(LiquidCrystal_I2C &lcd)
+{
+  lcd.clear();
+  lcd.backlight();
+  int seconds = 0;
+  lcd.setCursor(0, 0);
+  lcd.print("Tiempo:");
+  lcd.setCursor(13, 0);
+  unsigned int state = 0;
+  while (1)
+  {
+    lcd.print(seconds);
+    while (!state)
+      state = read_pins();
+    if ((1 & state) && (seconds > 5))
+      seconds -= 5;
+    else if ((2 & state) && (seconds < 994))
+      seconds += 5;
+    else if (4 & state)
+    {
+      delay(BUTTON_DELAY);
+      return seconds;
+    }
+    delay(BUTTON_DELAY);
+  }
+}
+
+// plays an entire game of charades
+void charades_game(LiquidCrystal_I2C &lcd)
+{
+}
