98dd4209b66fce6f462efbf629aa856841b6e8a9
Taciano <taciano3003@gmail.com>
2023-07-30 18:08:16 -0300

    solved start_game bug

--- game-station.ino
+++ game-station.ino
@@ -1,6 +1,7 @@
 #include "spy.h"
 #include "charades.h"
 #include "file-managing.h"
+#include <LiquidCrystal_I2C.h>
 
 #define CHARADES 1
 #define SPY 0
@@ -20,9 +21,9 @@ int start_menu(LiquidCrystal_I2C &lcd)
     if (state)
     {
       if (8 & state)
-        return CHARADES;
-      else if (4 & state)
         return SPY;
+      else if (4 & state)
+        return CHARADES;
     }
   }
 }
@@ -30,13 +31,14 @@ int start_menu(LiquidCrystal_I2C &lcd)
 void setup()
 {
   Serial.begin(115200);
-  LiquidCrystal_I2C lcd = LiquidCrystal_I2C(LCDPROP);
+  LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
+  lcd.init();
+  lcd.clear();
   pinMode(CS_PIN, OUTPUT);
   pinMode(BUTTON1, INPUT);
   pinMode(BUTTON2, INPUT);
   pinMode(BUTTON3, INPUT);
   pinMode(BUTTON4, INPUT);
-  start_menu(lcd);
   int game;
   while (1)
   {
